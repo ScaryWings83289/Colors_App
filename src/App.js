d3ceb8ed86f0833bed1b3db4d52cc0768b6016a9
DIVYANKAR BHARGAV <divyankarbhargav@gmail.com>
2020-07-25 12:46:44 +0530

    Add Palette footer

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
-    console.log(generatePalette(seedColors[4]))
+    console.log(generatePalette(seedColors[1]))
     return (
       <div>
-        <Palette palette={generatePalette(seedColors[4])} />
+        <Palette palette={generatePalette(seedColors[1])} />
       </div>
     );
   }
