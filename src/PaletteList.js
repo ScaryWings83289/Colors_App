e6b89caf87d48a4c675b7af5a61b1dbfc1fdc725
DIVYANKAR BHARGAV <divyankarbhargav@gmail.com>
2020-07-31 20:05:21 +0530

    Refactor with PureComponent

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -60,7 +60,7 @@ class PaletteList extends Component {
                             >
                                 <MiniPalette 
                                     {...palette} 
-                                    handleClick={() => this.goToPalette(palette.id)}
+                                    goToPalette={this.goToPalette}
                                     openDialog={this.openDialog}
                                     key={palette.id}
                                     id={palette.id}
